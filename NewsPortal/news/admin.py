1281a544a588e227e5403a3539d5fd72c90611f0
Dimmirion <dimas1995ru@gmail.com>
2025-07-24 15:16:20 +0300

    Add files via upload

--- NewsPortal/news/admin.py
+++ NewsPortal/news/admin.py
@@ -1,3 +1,22 @@
+# news/admin.py
 from django.contrib import admin
+from .models import Author, Category, Post, PostCategory, Comment
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'post_type', 'created_at', 'rating')
+    list_filter = ('post_type', 'created_at')
+    search_fields = ('title', 'content')
+
+admin.site.register(Post, PostAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user', 'created_at', 'rating')
+    list_filter = ('created_at',)
+    search_fields = ('text',)
+
+admin.site.register(Comment, CommentAdmin)
+
+# Простые регистрации для остальных моделей
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(PostCategory)
--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def news_list(request):
+    posts = Post.objects.filter(post_type=Post.NEWS).order_by('-created_at')
+    return render(request, 'news/news_list.html', {'posts': posts})
+
+def news_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'news/news_detail.html', {'post': post})
